7fb691e33b463a07c62ed51e3cc1f6607e657ead
ecknight <ellyknight@gmail.com>
2021-12-28 15:18:55 -0700

    Continued cleaning

--- 07_depart&arrive.R
+++ 07_depart&arrive.R
@@ -3,10 +3,6 @@ library(lubridate)
 
 options(scipen = 99999)
 
-#TO DO: DEAL WITH DEPARTURE & ARRIVAL OUTLIERS
-#TO DO: LOOK AT STOPOVER LENGTH MAX
-#TO DO: GO BACK AND DEAL WITH INCOMPLETE LEGS####
-#TO DO: CHECK # OF BIRDS####
 #TO DO: DECIDE WHAT TO DO WITH LONG STRETCHES WITH NO TRANSMISSIONS - worried about ones that overlap with departure or arrival
 
 #1. Read in data----
@@ -14,6 +10,9 @@ dat.raw <- read.csv("Data/LBCUFiltered&Predicted&Legged&Clustered&CPedData.csv")
   group_by(legid) %>% 
   mutate(nsdrecip = max(nsd) - nsd) %>% 
   ungroup()
+
+#number of birds
+length(unique(dat.raw$id))
 
 #2. Add ID for each cluster of HMM states----
 legs <- unique(dat.raw$legid)
@@ -34,22 +33,47 @@ for(i in 1:length(legs)){
   dat <- rbind(dat, dat.i)
 }
 
-#3. Look at one individual----
-dat.i <- dat %>% 
-  dplyr::select(legid) %>% 
-  unique() %>% 
-  sample_n(1) %>% 
-  left_join(dat) 
+length(unique(dat$id))
 
-ggplot(dat.i) +
-  geom_path(aes(x=X, y=Y)) +
-  geom_point(aes(x=X, y=Y, colour=nsd), size=3, alpha = 0.7) +
-  scale_colour_viridis_c() +
-  facet_grid(predictedState ~ cp)
+#3. Remove points where there's a huge gap in transmission that spans arrival & departure----
+dat.filter <- dat %>% 
+  dplyr::filter(!(legid=="172070515-2017-1spring" & doy %in% c(39:144)),
+                !(legid=="94034-2009-2fall" & doy %in% c(172:249)),
+                !(legid=="94033-2009-2fall" & doy %in% c(171:249)),
+                !(legid=="46768108-2015-1spring" & doy %in% c(71:93)),
+                !(legid=="46769588-2019-1spring" & doy %in% c(74:93)),
+                !(legid=="46769927-2014-1spring" & doy %in% c(100:118, 120:144, 146:148)),
+                !(legid=="46770723-2014-1spring" & doy %in% c(101:118, 120:143)))
 
-#3. Classify departure----
+#4. Split out legs where bird didn't start migration----
+#Also leg for where there's a huge transmission gap that spans arrival & departure
+dat.nodep <- dat.filter %>% 
+  group_by(legid) %>% 
+  summarize(nsdmax = max(nsd)) %>% 
+  ungroup() %>% 
+  dplyr::filter(nsdmax < 38036899358.37) %>% 
+  left_join(dat.filter) %>% 
+  rbind(dat.filter %>% 
+          dplyr::filter((legid=="172070515-2017-1spring" & doy > 144),
+                        (legid=="46768108-2015-1spring" & doy < 71),
+                        (legid=="46769588-2019-1spring" & doy < 74),
+                        (legid=="46769927-2014-1spring" & doy < 100),
+                        (legid=="46770723-2014-1spring" & doy < 101))) %>% 
+  mutate(season = case_when(legseason=="1spring" ~ "winter",
+                            legseason=="2fall" ~ "breed"),
+         season= ifelse(legid=="172070515-2017-1spring" & doy > 144, "breed", season),
+         season = ifelse(legid=="46768108-2015-1spring" & doy < 71, "winter", season),
+         season = ifelse(legid=="46769588-2019-1spring" & doy < 74, "winter", season),
+         season = ifelse(legid=="46769927-2014-1spring" & doy < 100, "winter", season),
+         season = ifelse(legid=="46770723-2014-1spring" & doy < 101, "winter", season))
+
+#Data for birds that did start migration to carry on to next step
+dat.dep <- dat.filter %>% 
+  anti_join(dat.nodep)
+
+#5. Classify departure----
 #departure = beginning of earliest state 2 cluster after nsd > 20000000000
-dat.depart <- dat %>% 
+dat.depart <- dat.dep %>% 
   dplyr::filter(predictedState==2,
                 nsd > 20000000000) %>% 
   group_by(legid) %>% 
@@ -58,22 +82,31 @@ dat.depart <- dat %>%
   ungroup() %>% 
   dplyr::select(legid, stateid) %>% 
   unique() %>% 
-  left_join(dat) %>% 
+  left_join(dat.dep) %>% 
   group_by(legid, stateid) %>% 
   arrange(date) %>% 
   filter(row_number()==1) %>% 
   ungroup() %>% 
   mutate(segment = "depart") %>% 
-  right_join(dat) %>% 
+  right_join(dat.dep) %>% 
   mutate(segment= case_when(legid=="164383320-2017-1spring" & doy==73 ~ "depart",
                             legid=="279282698-2019-1spring" & doy==74 ~ "depart",
-                            legid=="46769840-2015-1spring" & doy==75 ~ "depart",
+                            legid=="172070515-2017-2fall" & doy==169 ~ "depart",
+                            legid=="46769927-2014-2fall" & doy==181 ~ "depart",
+                            legid=="145696972-2016-2fall" & doy==170 ~ "depart",
+                            legid=="279818280-2018-1spring" & doy==99 ~ "depart",
                             !is.na(segment) ~ segment),
-         segment = ifelse(legid=="279282698-2019-1spring" & doy==33, NA, segment)) %>% 
+         segment = ifelse(legid=="279282698-2019-1spring" & doy %in% c(33,49), NA, segment),
+         segment = ifelse(legid=="172070515-2017-2fall" & doy==135, NA, segment),
+         segment = ifelse(legid=="46769927-2014-2fall" & doy==135, NA, segment),
+         segment = ifelse(legid=="145696972-2016-2fall" & doy==136, NA, segment),
+         segment = ifelse(legid=="279818280-2018-1spring" & doy==33, NA, segment)) %>% 
   arrange(id, legid, date)
 
+#these are individuals that the nsd rule doesn't work for because they were faffing around. I've done it manually instead, but still using the state==2 rule
+
 #check if # of departures is same as # of legids
-length(unique(dat$legid))
+length(unique(dat.dep$legid))
 table(dat.depart$segment)
 
 #check one per legid
@@ -83,13 +116,39 @@ depart.ids <- data.frame(table(dat.depart$legid, dat.depart$segment)) %>%
                 Freq > 1)
 nrow(depart.ids)
 
-dat.id <- dat.depart %>% 
-  dplyr::filter(legid %in% depart.ids$legid) %>% 
-  dplyr::select(id, legid, X, Y, date, doy, predictedState, stateid, stateidn, probState2, nsd, nsdrecip, dist, cp)
+#6. Filter out birds that didn't complete migration----
+dat.arr <- dat.depart %>%
+  dplyr::filter(!legid %in%  c("1418934379-2021-1spring",
+                               "172070515-2017-2fall",
+                               "99900-2021-2fall",
+                               "1146533212-2021-1spring",
+                               "1418878943-2021-1spring",
+                               "1418896449-2021-1spring",
+                               "99900-2021-2fall",
+                               "77639184-2016-2fall",
+                               "479364105-2019-1spring",
+                               "46768108-2016-2fall",
+                               "290352179-2020-2fall",
+                               "188150741-2021-1spring",
+                               "172070515-2019-2fall",
+                               "1418896449-2021-1spring",
+                               "1418878943-2021-1spring",
+                               "172070515-2017-1spring",
+                               "281981414-2018-1spring",
+                               "290347351-2019-2fall",
+                               "877974163-2020-1spring",
+                               "877974163-2020-1spring",
+                               "46768277-2014-2fall",
+                               "94034-2009-2fall",
+                               "94033-2009-2fall"))
 
-#4. Classify arrival----
-#arrival = first run after departure of stationary points > 10 days in length
-dat.migration <- dat.depart %>% 
+length(unique(dat.arr$legid))
+
+#7. Classify arrival----
+#arrival = first date of run of stationary points > 3 weeks after departure OR most first day of last run of stationary points
+
+#Fill in migration segment
+dat.migration <- dat.arr %>% 
   group_by(legid) %>% 
   mutate(segment2 = ifelse(lag(segment)=="depart", "migration", segment)) %>% 
   tidyr::fill(segment2, .direction="down") %>% 
@@ -99,33 +158,61 @@ dat.migration <- dat.depart %>%
 
 table(dat.migration$segment)
 
-dat.arrive <- dat.migration %>% 
-  mutate(segment = ifelse(segment=="migration" & predictedState==1 & stateidn > 7, "arrive", segment)) %>% 
-  dplyr::filter(segment=="arrive") %>% 
+#Classify arrival via stopover rule
+dat.arrive.stopover <- dat.migration %>% 
+  mutate(segment2 = ifelse(segment=="migration" & predictedState==1 & stateidn > 30, "arrive", segment)) %>% 
+  dplyr::filter(segment2=="arrive") %>% 
   group_by(legid) %>% 
   arrange(date) %>% 
   filter(row_number()==1) %>% 
   ungroup() %>% 
-  right_join(dat) %>% 
-  mutate(segment= case_when(legid=="281980840-2020-2fall" & doy==180 ~ "arrive",
+  right_join(dat.migration) %>% 
+  mutate(segment2= case_when(legid=="281980840-2020-2fall" & doy==180 ~ "arrive",
                             legid=="477994534-2018-2fall" & doy==190 ~ "arrive",
-                            !is.na(segment) ~ segment))
+                            legid=="172070515-2016-2fall" & doy==193 ~ "arrive",
+                            legid=="172070515-2017-2fall" & doy==176 ~ "arrive",
+                            !is.na(segment2) ~ segment2),
+         segment = ifelse(!is.na(segment2), segment2, segment)) %>% 
+  arrange(id, date) %>% 
+  dplyr::select(-segment2)
 
-table(dat.arrive$segment)
+#Check number classified
+table(dat.arrive.stopover$segment)
 
 #Find missing ids
-arrive.ids <- dat.depart %>% 
+arrive.ids <- dat.arr %>% 
   dplyr::select(legid) %>% 
   unique() %>% 
-  anti_join(dat.arrive %>% 
+  anti_join(dat.arrive.stopover %>% 
               dplyr::filter(segment=="arrive") %>% 
               dplyr::select(legid) %>% 
               unique())
 nrow(arrive.ids)
 
-dat.id <- dat.depart %>% 
-  dplyr::filter(legid %in% arrive.ids$legid) %>% 
-  dplyr::select(id, legid, X, Y, date, doy, predictedState, stateid, stateidn, probState2, nsd, nsdrecip, dist, cp, segment)
+#Classify arrival for remaining ids using last segment rule
+dat.arrive.last <- dat.migration %>% 
+  dplyr::filter(legid %in% arrive.ids$legid,
+                predictedState==1) %>% 
+  group_by(legid) %>% 
+  dplyr::filter(stateid==max(stateid)) %>% 
+  arrange(date) %>% 
+  filter(row_number()==1) %>% 
+  ungroup() %>% 
+  mutate(segment2="arrive") %>% 
+  right_join(dat.migration %>% 
+     dplyr::filter(legid %in% arrive.ids$legid)) %>% 
+  mutate(segment = ifelse(!is.na(segment2), segment2, segment)) %>% 
+  arrange(id, date) %>% 
+  dplyr::select(-segment2)
+
+table(dat.arrive.last$segment)
+
+#8. Put two arrival classifications back together----
+dat.arrive <- dat.arrive.stopover %>% 
+  dplyr::filter(!legid %in% arrive.ids$legid) %>% 
+  rbind(dat.arrive.last)
+
+table(dat.arrive$segment)
 
 #check one per legid
 depart.ids <- data.frame(table(dat.arrive$legid, dat.arrive$segment)) %>% 
@@ -134,12 +221,12 @@ depart.ids <- data.frame(table(dat.arrive$legid, dat.arrive$segment)) %>%
                 Freq > 1)
 nrow(depart.ids)
 
-#5. Fill in the gaps----
+#9. Fill in the gaps----
 dat.arrivedepart <- dat.depart %>% 
   dplyr::filter(segment=="depart") %>% 
   rbind(dat.arrive %>% 
           dplyr::filter(segment=="arrive")) %>% 
-  right_join(dat) %>% 
+  right_join(dat.dep) %>% 
   arrange(legid, date) %>% 
   mutate(segment2 = case_when(row_number()==1 ~ "stationary",
                              lead(segment)=="depart" ~ "stationary",
@@ -154,27 +241,32 @@ dat.arrivedepart <- dat.depart %>%
   ungroup() %>% 
   mutate(segment = case_when(legid=="164383320-2017-1spring" & doy==73 ~ "depart",
                              legid=="164383320-2017-1spring" & doy==74 ~ "arrive",
-                             legid=="46769840-2015-1spring" & doy==75 ~ "depart",
-                             legid=="46769840-2015-1spring" & doy==76 ~ "arrive",
+                             legid=="46694077-2014-1spring" & doy==93 ~ "depart",
+                             legid=="46694077-2014-1spring" & doy==102 ~ "arrive",
                              !is.na(segment) ~ segment))
+
+#tidying above is birds that depart and arrive on consecutive days
 
 table(dat.arrivedepart$segment)
 
-arrivedepart.ids <- dat.arrive %>% 
+#Identify missing legids
+arrivedepart.ids <- dat.depart %>% 
   dplyr::select(legid) %>% 
   unique() %>% 
   anti_join(dat.arrivedepart %>% 
-              dplyr::filter(segment=="arrive") %>% 
+              dplyr::filter(segment=="depart") %>% 
               dplyr::select(legid) %>% 
               unique())
 nrow(arrivedepart.ids)
 
-dat.id <- dat.arrivedepart %>% 
-  arrange(legid, date) %>% 
-  dplyr::filter(legid %in% arrivedepart.ids$legid) %>% 
-  dplyr::select(id, legid, X, Y, date, doy, predictedState, stateid, stateidn, probState2, nsd, nsdrecip, dist, cp, segment)
+#Check for one per legid
+arrivedepart.ids <- data.frame(table(dat.arrivedepart$legid, dat.arrivedepart$segment)) %>% 
+  rename(legid = Var1, segment= Var2) %>% 
+  dplyr::filter(segment=="depart",
+                Freq > 1)
+nrow(arrivedepart.ids)
 
-#6. Classify to season---
+#10. Classify to season---
 dat.season <- dat.arrivedepart %>% 
   mutate(season=case_when(segment=="migration" & legseason=="1spring" ~ "springmig",
                           segment=="migration" & legseason=="2fall" ~ "fallmig",
@@ -184,10 +276,12 @@ dat.season <- dat.arrivedepart %>%
                              lead(season)=="depart" & legseason=="2fall" ~ "breed",
                              lag(season)=="arrive" & legseason=="2fall" ~ "winter",
                              !is.na(season) ~ season)) %>% 
-  mutate(season = ifelse(season=="stationary", NA, season)) %>% 
+  mutate(season=ifelse(season %in% c("springmig", "fallmig") & predictedState==1, "stopover", season),
+         season = ifelse(season=="stationary", NA, season)) %>% 
   group_by(id) %>% 
   fill(season, .direction="updown") %>% 
-  dplyr::select(id, legid, X, Y, date, doy, predictedState, stateid, stateidn, probState2, nsd, nsdrecip, dist, cp, segment, legseason, season)
+  dplyr::select(id, legid, lat, lon, X, Y, date, doy, predictedState, stateid, stateidn, probState2, nsd, nsdrecip, dist, cp, segment, legseason, season) %>% 
+  ungroup()
   
 table(dat.season$season)
 
@@ -199,12 +293,49 @@ dat.na <- dat.season %>%
   left_join(dat.season)
 nrow(dat.na)
 
-#7. Visualize----
-ids <- unique(dat.season$id)
+#10. Add birds that never left back in----
+dat.all <- dat.nodep %>% 
+  mutate(segment = NA) %>% 
+  dplyr::select(id, legid, lat, lon, X, Y, date, doy, predictedState, stateid, stateidn, probState2, nsd, nsdrecip, dist, cp, segment, legseason, season) %>% 
+  rbind(dat.season)
+
+#11. Tidy----
+dat.tidy <- dat.season %>% 
+  mutate(season = case_when(legid=="279818280-2017-2fall" & doy > 191 ~ "winter",
+                            legid=="172070515-2016-2fall" & doy == 193 ~ "arrive",
+                            legid=="172070515-2016-2fall" & doy > 193 ~ "winter",
+                            legid=="172070515-2016-2fall" & doy <= 32 ~ "winter",
+                            legid=="890858132-2019-2fall" & doy == 205 ~ "arrive",
+                            legid=="890858132-2019-2fall" & doy > 205 ~ "winter",
+                            legid=="71767680-2016-2fall" & doy==169 ~ "arrive",
+                            legid=="71767680-2016-2fall" & doy > 169 ~ "winter",
+                            legid=="145696972-2016-2fall" & doy==155 ~ "depart",
+                            legid=="145696972-2016-2fall" & doy < 155 ~ "breed",
+                            legid=="279818280-2018-1spring" & doy==89 ~ "depart",
+                            legid=="279818280-2018-1spring" & doy < 89 ~ "winter",
+                            legid=="94034-2009-2fall" & doy >= 250 ~ "winter",
+                            legid=="94033-2009-2fall" & doy >= 250 ~ "winter",
+                            legid=="281981414-2018-1spring" & doy== 93 ~ "winter",
+                            legid=="46768108-2015-1spring" & doy %in% c(69:70) ~ "winter",
+                            legid=="46769588-2019-1spring" & doy <= 73 ~ "winter",
+                            legid=="46769588-2019-1spring" & doy ==94 ~ "breed",
+                            legid=="46769927-2014-1spring" & doy %in% c(119, 145) ~ "springmig",
+                            legid=="46769927-2014-1spring" & doy >= 149 ~ "breed",
+                            legid=="46769927-2014-1spring" & doy <= 99 ~ "winter",
+                            legid=="46770723-2014-1spring" & doy == 119 ~ "springmig",
+                            legid=="46770723-2014-1spring" & doy < 119 ~ "winter",
+                            !is.na(season) ~ season))
+
+ggplot(dat.tidy %>% dplyr::filter(season %in% c("depart", "arrive"))) +
+  geom_histogram(aes(x=doy)) +
+  facet_grid(season~legseason)
+
+#12. Visualize----
+ids <- unique(dat.tidy$id)
 
 for(i in 1:length(ids)){
   
-  dat.i <- dat.season %>% 
+  dat.i <- dat.tidy %>% 
     dplyr::filter(id==ids[i])
   
   ggplot(dat.i) +
@@ -217,8 +348,8 @@ for(i in 1:length(ids)){
   
 }
 
-#8. Summary stats of dates----
-dates <- dat.season %>% 
+#13. Summary stats of dates----
+dates <- dat.tidy %>% 
   dplyr::filter(season %in% c("depart", "arrive")) %>% 
   group_by(legseason, season) %>% 
   summarize(mean = mean(doy),
@@ -227,10 +358,9 @@ dates <- dat.season %>%
   ungroup()
 dates
 
-ggplot(dat.season %>% dplyr::filter(season %in% c("depart", "arrive"))) +
-  geom_histogram(aes(x=doy)) +
-  facet_grid(season~legseason)
-#definitely still some outliers
-
-ggplot(dat.season) +
+ggplot(dat.tidy) +
   geom_point(aes(x=X, y=Y, colour=season))
+
+
+#14. Write out----
+write.csv(dat.tidy, "Data/LBCUSegmentedData.csv", row.names = FALSE)
