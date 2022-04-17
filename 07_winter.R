6bc300ac92495b529bc3d68e972a27dc1b100dca
ecknight <ellyknight@gmail.com>
2022-04-17 13:25:56 -0600

    Remove stopovers < 100 km from breeding/wintering grounds

--- 07_winter.R
+++ 07_winter.R
@@ -2,6 +2,9 @@ library(tidyverse)
 library(lubridate)
 library(moveHMM)
 library(meanShiftR)
+library(sf)
+library(adehabitatLT)
+library(data.table)
 
 options(scipen = 99999)
 
@@ -20,7 +23,8 @@ dat.ll <- dat %>%
   dplyr::select(winterid, lon, lat) %>% 
   rename(ID=winterid)
 
-md <- prepData(dat.ll, coordNames = c("lon","lat"), type="LL")
+md <- prepData(dat.ll, coordNames = c("lon","lat"), type="LL") %>% 
+  mutate(step = ifelse(step==0, 0.001, step))
 
 #3. Choose starting parameters----
 ## Visualize to choose initial parameters
@@ -119,17 +123,60 @@ dat.tidy <- dat.wint %>%
                             id=="1418878943" & winter=="winter3" ~ "winter2",
                             id=="279818280" & winter %in% c("winter2", "wintermig") ~ "winter2",
                             !is.na(winter) ~ winter)) %>% 
-  dplyr::select(study, id, sensor, sex, mass, lat, lon, X, Y, year, date, doy, segment, season, stopover, winter)
+  dplyr::select(study, id, sensor, sex, mass, lat, lon, X, Y, year, date, doy, segment, season, stopover, stopovercluster,winter)
 
 #11. Put back together with other data----
 dat.all <- dat.raw %>% 
   dplyr::filter(season!="winter") %>% 
   mutate(winter = "other") %>% 
-  dplyr::select(study, id, sensor, sex, mass, lat, lon, X, Y, year, date, doy, segment, season, stopover, winter) %>% 
+  dplyr::select(study, id, sensor, sex, mass, lat, lon, X, Y, year, date, doy, segment, season, stopover, stopovercluster, winter) %>% 
   rbind(dat.tidy) 
 
 table(dat.all$season, dat.all$winter)
 
+#12. Remove stopovers less than 100 km from breeding & wintering grounds----
+distthresh <- 100
+
+dat.dist <- dat.all %>% 
+  group_by(id, year) %>% 
+  dplyr::filter((season %in% c("fallmig", "springmig") & stopover==1) |
+                  (season %in% c("breed", "winter") & lag(season) %in% c("fallmig", "springmig")) |
+                  (season %in% c("breed", "winter") & lead(season) %in% c("fallmig", "springmig"))) %>% 
+  arrange(id, date) %>% 
+  group_by(id, year, season, stopovercluster) %>% 
+  summarize(X = mean(X), 
+            Y = mean(Y)) %>% 
+  mutate(order = row_number()) %>% 
+  ungroup() %>% 
+  data.frame()
+
+dat.traj <- as.ltraj(xy=dat.dist[,c("X", "Y")],
+                 id=dat.dist$id,
+                 date=dat.dist$order,
+                 typeII=FALSE,
+                 proj4string = CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs")) %>% 
+  rbindlist() %>% 
+  dplyr::select(-date) %>% 
+  cbind(dat.dist) %>% 
+  dplyr::select(id, year, season, stopovercluster, X, Y, dist) %>% 
+  mutate(dist = dist/1000)
+
+dat.remove <- dat.traj %>% 
+  dplyr::filter((season=="fallmig" & lag(season=="breed") & lag(dist) < distthresh) |
+                (season=="fallmig" & lead(season=="winter") & dist < distthresh) |
+                  (season=="springmig" & lag(season=="winter") & lag(dist) < distthresh) |
+                  (season=="springmig" & lead(season=="breed" & dist < distthresh))) %>% 
+  mutate(remove=1) 
+
+dat.final <- dat.remove %>% 
+  dplyr::select(id, year, season, stopovercluster, remove) %>% 
+  full_join(dat.all) %>% 
+  mutate(remove = ifelse(is.na(remove), 0, remove),
+         stopover=ifelse(remove==1, 0, stopover),
+         stopovercluster = ifelse(remove==1, NA, stopovercluster)) %>% 
+  dplyr::select(-remove)
+  
 #12. Save----
-write.csv(dat.all, "Data/LBCU_FilteredData_Segmented.csv", row.names = FALSE)
-write.csv(dat.all, "/Users/ellyknight/Dropbox/LBCU/LBCU_FilteredData_Segmented.csv", row.names = FALSE)
+write.csv(dat.final, "Data/LBCU_FilteredData_Segmented.csv", row.names = FALSE)
+write.csv(dat.final, "/Users/ellyknight/Dropbox/LBCU/LBCU_FilteredData_Segmented.csv", row.names = FALSE)
+write.csv(dat.final, "/Users/ellyknight/Documents/SMBC/Analysis/lbcu_mc/Data/LBCU_FilteredData_Segmented.csv", row.names = FALSE)
