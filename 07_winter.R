ea65e913b7de1de866658dd8591e10812bb010e9
ecknight <ellyknight@gmail.com>
2022-07-21 09:52:19 -0600

    redo the tidying

--- 07_winter.R
+++ 07_winter.R
@@ -99,7 +99,7 @@ dat.wint <- dat.shift.n %>%
   right_join(dat.shift.n) %>% 
   mutate(winter = ifelse(is.na(winter), "wintermig", paste0("winter", winter)))
 
-#10. Assess birds with multiple home ranges----
+#10. Assess & tidy birds with multiple home ranges----
 dat.n <- dat.wint %>% 
   dplyr::select(id, wintern) %>% 
   unique()
@@ -108,21 +108,35 @@ table(dat.n$wintern)
   
 #Look at birds with three home ranges
 dat.3 <- dat.wint %>% 
-  dplyr::filter(wintern=="3") %>% 
+  dplyr::filter(wintern>2) %>% 
   dplyr::select(id) %>% 
   unique()
-#973658514 - should perhaps collapse winter1, winter2, & wintermig
-#1418878943 - should perhaps collapse winter 1 & 2
+dat.3
+#973658514 - should perhaps collapse winter2, winter3, & wintermig
+#77637122 - collapse winter2 & winter 3
+#1418878943 - collapse winter 2 & 3
 #1419060032 - 3 looks appropriate
-#279818280 - collapse winter 2 & wintermig
-#1615638072 - 3 loops appropriate
+#1953212672 = collapse winter 2 & 3
+#1953212678 = collapse winter 2 & 3
+#1953212680 = collapse winter 2 & 3
+#1953212691 = collapse winter 2 & 3
+
+#ggplot(dplyr::filter(dat.wint, id %in% dat.3$id)) +
+ggplot(dplyr::filter(dat.wint, id==1953212691)) +  
+  geom_point(aes(x=lon, y=lat, colour=factor(winter))) +
+  facet_wrap(~id, scales="free")
 
 dat.tidy <- dat.wint %>% 
-  mutate(winter = case_when(id=="973658514" & winter %in% c("winter1", "winter2", "wintermig") ~ "winter1",
-                            id=="973658514" & winter=="winter3" ~ "winter2",
-                            id=="1418878943" & winter %in% c("winter1", "winter2") ~ "winter1",
+  mutate(winter = case_when(id=="973658514" & winter %in% c("winter2", "winter3") ~ "winter2",
+                            id=="973658514" & winter=="winter4" ~ "winter3",
+                            id=="973658514" & winter=="winter5" ~ "winter4",
+                            id=="973658514" & winter=="winter6" ~ "wintermig",
+                            id=="77637122" & winter=="winter3" ~ "winter2",
                             id=="1418878943" & winter=="winter3" ~ "winter2",
-                            id=="279818280" & winter %in% c("winter2", "wintermig") ~ "winter2",
+                            id=="1953212672" & winter=="winter3" ~ "winter2",
+                            id=="1953212678" & winter=="winter3" ~ "winter2",
+                            id=="1953212680" & winter=="winter3" ~ "winter2",
+                            id=="1953212691" & winter=="winter3" ~ "winter2",
                             !is.na(winter) ~ winter)) %>% 
   dplyr::select(study, id, sensor, sex, mass, lat, lon, X, Y, year, date, doy, segment, season, stopover, stopovercluster, winter)
 
@@ -135,48 +149,6 @@ dat.all <- dat.raw %>%
 
 table(dat.all$season, dat.all$winter)
 
-#12. Remove stopovers less than 100 km from breeding & wintering grounds----
-# distthresh <- 100
-# 
-# dat.dist <- dat.all %>% 
-#   group_by(id, year) %>% 
-#   dplyr::filter((season %in% c("fallmig", "springmig") & stopover==1) |
-#                   (season %in% c("breed", "winter") & lag(season) %in% c("fallmig", "springmig")) |
-#                   (season %in% c("breed", "winter") & lead(season) %in% c("fallmig", "springmig"))) %>% 
-#   arrange(id, date) %>% 
-#   group_by(id, year, season, stopovercluster) %>% 
-#   summarize(X = mean(X), 
-#             Y = mean(Y)) %>% 
-#   mutate(order = row_number()) %>% 
-#   ungroup() %>% 
-#   data.frame()
-# 
-# dat.traj <- as.ltraj(xy=dat.dist[,c("X", "Y")],
-#                  id=dat.dist$id,
-#                  date=dat.dist$order,
-#                  typeII=FALSE,
-#                  proj4string = CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs")) %>% 
-#   rbindlist() %>% 
-#   dplyr::select(-date) %>% 
-#   cbind(dat.dist) %>% 
-#   dplyr::select(id, year, season, stopovercluster, X, Y, dist) %>% 
-#   mutate(dist = dist/1000)
-# 
-# dat.remove <- dat.traj %>% 
-#   dplyr::filter((season=="fallmig" & lag(season=="breed") & lag(dist) < distthresh) |
-#                 (season=="fallmig" & lead(season=="winter") & dist < distthresh) |
-#                   (season=="springmig" & lag(season=="winter") & lag(dist) < distthresh) |
-#                   (season=="springmig" & lead(season=="breed" & dist < distthresh))) %>% 
-#   mutate(remove=1) 
-# 
-# dat.final <- dat.remove %>% 
-#   dplyr::select(id, year, season, stopovercluster, remove) %>% 
-#   full_join(dat.all) %>% 
-#   mutate(remove = ifelse(is.na(remove), 0, remove),
-#          stopover=ifelse(remove==1, 0, stopover),
-#          stopovercluster = ifelse(remove==1, NA, stopovercluster)) %>% 
-#   dplyr::select(-remove)
-  
 #12. Save----
 write.csv(dat.all, "Data/LBCU_FilteredData_Segmented.csv", row.names = FALSE)
 write.csv(dat.all, "/Users/ellyknight/Dropbox/LBCU/LBCU_FilteredData_Segmented.csv", row.names = FALSE)
